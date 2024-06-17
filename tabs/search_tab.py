53e319764711a9947c86df544eb596ef176c9817
Hariel <harielpimenta@hotmail.com>
2024-06-17 20:08:34 -0300

    Updates in search tab

--- tabs/search_tab.py
+++ tabs/search_tab.py
@@ -20,24 +20,68 @@ class SearchProductFrame(ctk.CTkFrame):
         
         
         # Sample data to be displayed in the Treeview
-        list_names = [['0', 'Hariel', 'harie@hotmail.com'], ['1', 'Beatriz', 'bea@gmail.com'], ['2', 'Miguel', 'miguelpatrulha@gmail.com']]
+        list_names = list_names = [
+    ['1', 'T-shirt', 'Clothing', 'M', 'Unisex', 'Nike', '$15.00', '$30.00'],
+    ['2', 'Jeans', 'Clothing', 'L', 'Male', 'Levi\'s', '$25.00', '$50.00'],
+    ['3', 'Dress', 'Clothing', 'S', 'Female', 'Adidas', '$20.00', '$40.00'],
+    ['4', 'Shoes', 'Footwear', '42', 'Male', 'Puma', '$30.00', '$60.00'],
+    ['5', 'Hat', 'Accessory', 'M', 'Unisex', 'Nike', '$10.00', '$20.00'],
+    ['6', 'Skirt', 'Clothing', 'M', 'Female', 'Zara', '$18.00', '$36.00'],
+    ['7', 'Jacket', 'Clothing', 'L', 'Unisex', 'H&M', '$35.00', '$70.00'],
+    ['8', 'Sandals', 'Footwear', '38', 'Female', 'Gucci', '$22.00', '$44.00'],
+    ['9', 'Socks', 'Accessory', 'L', 'Male', 'Puma', '$5.00', '$10.00'],
+    ['10', 'Sweater', 'Clothing', 'M', 'Female', 'Adidas', '$28.00', '$56.00'],
+    ['11', 'Scarf', 'Accessory', 'One Size', 'Unisex', 'Gucci', '$12.00', '$24.00'],
+    ['12', 'Shorts', 'Clothing', 'M', 'Male', 'Nike', '$15.00', '$30.00'],
+    ['13', 'Belt', 'Accessory', 'L', 'Male', 'Levi\'s', '$8.00', '$16.00'],
+    ['14', 'Blouse', 'Clothing', 'S', 'Female', 'H&M', '$20.00', '$40.00'],
+    ['15', 'Boots', 'Footwear', '40', 'Unisex', 'Dr. Martens', '$50.00', '$100.00']
+]
+
 
 
         self.tree_view_frame = ctk.CTkFrame(self, width=800, height=400)
-        tree_view = ttk.Treeview(self.tree_view_frame, columns=('id', 'name', 'email'), show='headings')
-        tree_view.column('id', minwidth=50, width=200)
-        tree_view.column('name', minwidth=50, width=300)
-        tree_view.column('email', minwidth=50, width=300)
+        tree_view = ttk.Treeview(self.tree_view_frame, columns=('id', 'product', 'product_type', 'sizes', 'gender_product', 'brand', 'buying_price', 'selling_price'), show='headings')
+        tree_view.column('id', minwidth=100, width=100)
+        tree_view.column('product', minwidth=100, width=100)
+        tree_view.column('product_type', minwidth=100, width=100)
+        tree_view.column('sizes', minwidth=100, width=100)
+        tree_view.column('gender_product', minwidth=100, width=100)
+        tree_view.column('brand', minwidth=100, width=100)
+        tree_view.column('buying_price', minwidth=100, width=100)
+        tree_view.column('selling_price', minwidth=100, width=100)
+        
         tree_view.heading('id', text='ID')
-        tree_view.heading('name', text='NAME')
-        tree_view.heading('email', text='EMAIL')
+        tree_view.heading('product', text='PRODUCT')
+        tree_view.heading('product_type', text='TYPE PRODUCT')
+        tree_view.heading('sizes', text='SIZE')
+        tree_view.heading('gender_product', text='GENDER')
+        tree_view.heading('brand', text='BRAND')
+        tree_view.heading('buying_price', text='PAY PRICE')
+        tree_view.heading('selling_price', text='SELL PRICE')
         
-        for (i, n, e) in list_names:
-                tree_view.insert("", "end", values=(i, n, e))
-        
-        
-        
-        
+        for item in list_names:
+                tree_view.insert("", "end", values=item)
+
+        style = ttk.Style()
+    
+        style.theme_use("default")
+
+        style.configure("Treeview",
+                        background="black",
+                        foreground="white",
+                        rowheight=25,
+                        fieldbackground="black",
+                        borderwidth=0)
+        style.map('Treeview', background=[('selected', '#006400')])
+
+        style.configure("Treeview.Heading",
+                        background="#565b5e",
+                        foreground="white",
+                        relief="flat")
+        style.map("Treeview.Heading",
+                    background=[('active', 'green')])
+
         
         tree_view.pack(expand=True, fill='both')
         
