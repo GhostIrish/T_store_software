b372ee0e1a008018a8e30156776ef70120172687
Hariel <harielpimenta@hotmail.com>
2024-06-24 17:18:16 -0300

    I make new ui design for add_tab, i'm close to finish the first prototype of this software, i'm so excited :)

--- tabs/add_tab.py
+++ tabs/add_tab.py
@@ -3,26 +3,50 @@ import customtkinter as ctk
 class AddProductFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.create_widgets()
-
-    def create_widgets(self):
+        self.setup_ui()
+        
+    def setup_ui(self):
+        self.setup_label()
+        self.setup_field()
+        self.setup_option_widget()
+        self.setup_entrys()
+        self.setup_btn()
+        
+    def setup_label(self):
         label_add = ctk.CTkLabel(self, text="Add New Product", text_color="white", font=("Arial", 20))
         label_add.pack(pady=20, padx=20)
-
-        fields_frame = ctk.CTkFrame(self, fg_color="transparent")
-        fields_frame.pack(pady=20, padx=20, fill="both", expand=True)
-
-        labels = ["Product ", "Type ", "Size ", "Gender ", "Brand ", "Buying price ", "Selling price ", "Quantity "]
+    
+    def setup_field(self):
+        self.fields_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.fields_frame.pack(pady=20, padx=100, fill="both", expand=True)   
+    
+    def setup_entrys(self):
+        labels = ["Product ", "Buying price ", "Selling price ", "Quantity "]
         self.entries = []
 
         for i, label in enumerate(labels):
-            lbl = ctk.CTkLabel(fields_frame, text=label, text_color="white")
+            lbl = ctk.CTkLabel(self.fields_frame, text=label, text_color="white")
             lbl.grid(row=i, column=0, padx=10, pady=5, sticky="e")
 
-            entry = ctk.CTkEntry(fields_frame, placeholder_text=f"Write {label.lower()}", width=800)
+            entry = ctk.CTkEntry(self.fields_frame, placeholder_text=f"Write {label.lower()}", width=400)
             entry.grid(row=i, column=1, padx=10, pady=5, sticky="ew")
             self.entries.append(entry)
+            
+                    # Adding an empty column for spacing
+            self.fields_frame.grid_columnconfigure(2, minsize=50)  # Adjust the minsize as needed
+        
+    def setup_option_widget(self):
+        titles = ["Type ", "Size ", "Gender ", "Brand "]
+        for i, title in enumerate(titles):
+            lbl = ctk.CTkLabel(self.fields_frame, text=title, text_color="white")
+            lbl.grid(row=i, column=3, padx=10, pady=5, sticky="e")
+            
+            option_var = ctk.StringVar(value=f"{title}")
+            option_box = ctk.CTkOptionMenu(master=self.fields_frame, values=["teste1", "teste2"], width=150,
+                                           variable=option_var)
+            option_box.grid(row=i, column=4, padx=10, pady=5, sticky="ew")
 
+    def setup_btn(self):
         send_button = ctk.CTkButton(self, text="Send")
         send_button.pack(pady=20, padx=20)
 
--- tabs/testt.py
+++ tabs/testt.py
@@ -1,107 +1,20 @@
 import customtkinter as ctk
-from tkinter import ttk
-import requests
 
-class SearchProductFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-        label_search = ctk.CTkLabel(self, text="Search Product", text_color="white", font=("Arial", 20))
-        label_search.pack(pady=20, padx=20)
-
-        search_frame = ctk.CTkFrame(self, fg_color="transparent")
-        search_frame.pack(pady=10)
-
-        self.search_bar = ctk.CTkEntry(search_frame, width=600, placeholder_text='Product to search')
-        self.search_bar.pack(side="left", padx=5)
-
-        btn = ctk.CTkButton(search_frame, width=100, text='Search', command=self.search_products)
-        btn.pack(side="left", padx=5)
-
-        self.tree_view_frame = ctk.CTkFrame(self, width=850, height=400)
-        self.tree_view = ttk.Treeview(self.tree_view_frame, columns=('id', 'product', 'product_type', 'sizes', 'gender_product', 'brand', 'buying_price', 'selling_price', 'quantity'), show='headings')
-        self.tree_view.column('id', minwidth=50, width=50)
-        self.tree_view.column('product', minwidth=100, width=100)
-        self.tree_view.column('product_type', minwidth=100, width=100)
-        self.tree_view.column('sizes', minwidth=100, width=100)
-        self.tree_view.column('gender_product', minwidth=100, width=100)
-        self.tree_view.column('brand', minwidth=100, width=100)
-        self.tree_view.column('buying_price', minwidth=100, width=100)
-        self.tree_view.column('selling_price', minwidth=100, width=100)
-        self.tree_view.column('quantity', minwidth=100, width=100)
-        
-        self.tree_view.heading('id', text='ID')
-        self.tree_view.heading('product', text='PRODUCT')
-        self.tree_view.heading('product_type', text='TYPE PRODUCT')
-        self.tree_view.heading('sizes', text='SIZE')
-        self.tree_view.heading('gender_product', text='GENDER')
-        self.tree_view.heading('brand', text='BRAND')
-        self.tree_view.heading('buying_price', text='PAY PRICE')
-        self.tree_view.heading('selling_price', text='SELL PRICE')
-        self.tree_view.heading('quantity', text='QUANTITY')
-
-        style = ttk.Style()
+window = ctk.CTk()
+window.geometry("1280x720")
+window.title("StyleHub - DashBoard")
+window.resizable(False, False)
     
-        style.theme_use("default")
 
-        style.configure("Treeview",
-                        background="black",
-                        foreground="white",
-                        rowheight=25,
-                        fieldbackground="black",
-                        borderwidth=0)
-        style.map('Treeview', background=[('selected', '#006400')])
+optionmenu_var = ctk.StringVar(value="option 2")  # set initial value
 
-        style.configure("Treeview.Heading",
-                        background="#565b5e",
-                        foreground="white",
-                        relief="flat")
-        style.map("Treeview.Heading",
-                    background=[('active', 'green')])
+def optionmenu_callback(choice):
+    print("optionmenu dropdown clicked:", choice)
 
-        
-        self.tree_view.pack(expand=True, fill='both')
-        
-        # Create and pack the frame for the Treeview, with fixed size
-        self.tree_view_frame.pack()
-        self.tree_view_frame.pack_propagate(False)
-        
-        self.search_products()
-    
-    def search_products(self):
-        global tree_view
-        query = self.search_bar.get()
-        # Fazer a requisição e inserir os produtos no Textbox
-        try:
-            if query:
-                url = f'http://localhost:5000/api/products?query={query}'
-            else:
-                url = 'http://localhost:5000/api/products'
-            
-            reponse = requests.get(url)
-            products = response.json()
-            print(products)
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred: {e}")
-        
-        for item in self.tree_view.get_children():
-            self.tree_view.delete(item)
-        
-        for product in products:
-            products = response.json()
-            product_values = (
-                product['id'], product['model_product'], product['product_type'], 
-                product['size'], product['gender'], product['brand'], 
-                product['buying_price'], product['selling_price'], product['quantity']
-            )
-            self.tree_view.insert("", "end", values=product_values)
+combobox = ctk.CTkOptionMenu(master=window,
+                                       values=["option 1", "option 2"],
+                                       command=optionmenu_callback,
+                                       variable=optionmenu_var)
+combobox.pack(padx=20, pady=10)
 
-        
-        
-        # def print_search_query(self):
-        #     search_query = self.search_bar.get()
-        #     print(f"Search query: {search_query}")
-        
-        
-        
-      
+window.mainloop()
