9a3d0aaa68fd428a74926493d20f9455164ea53a
Hariel <harielpimenta@hotmail.com>
2024-07-20 12:56:26 -0300

    testing docker 2

--- setup.bat
+++ setup.bat
@@ -1,26 +1,26 @@
-@echo off
-echo ================================
-echo  Iniciando Setup do Projeto
-echo ================================
+#!/bin/bash
 
-REM Diretório onde o projeto será clonado
-SET PROJECT_DIR=C:\project_t_store
+echo "=============================="
+echo " Iniciando Setup do Projeto"
+echo "=============================="
 
-REM URL do repositório Git
-SET REPO_URL=https://github.com/GhostIrish/T_store_software.git
+# Diretório onde o projeto será clonado
+PROJECT_DIR="/home/$(whoami)/project_t_store"
 
-REM Clonando o repositório
-echo Clonando o repositório do GitHub...
-git clone %REPO_URL% %PROJECT_DIR%
+# URL do repositório Git
+REPO_URL="https://github.com/GhostIrish/T_store_software.git"
 
-REM Navegando para o diretório do projeto
-cd %PROJECT_DIR%
+# Clonando o repositório
+echo "Clonando o repositório do GitHub..."
+git clone "$REPO_URL" "$PROJECT_DIR"
 
-REM Iniciando Docker Compose
-echo Iniciando serviços do Docker...
+# Navegando para o diretório do projeto
+cd "$PROJECT_DIR" || exit
+
+# Iniciando Docker Compose
+echo "Iniciando serviços do Docker..."
 docker-compose up -d
 
-echo ================================
-echo Setup concluído com sucesso!
-echo ================================
-pause
+echo "=============================="
+echo "Setup concluído com sucesso!"
+echo "=============================="
